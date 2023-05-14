55d7ec3f47ff8cbdc89f43a00f61814f9b41959a
Soumaila <diarra176@gmail.com>
2023-05-14 22:41:31 +0200

    ✨(feat) added main router to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,11 @@
 import express from 'express';
-import {Router} from 'express';
+import {MainRouter} from './routes/index';
 
 const app = express();
 const port = 3000;
-const router: Router = Router();
-
-router.get('/', (req, res) => {
-    res.send({'welcome': 'Hello and welcome to our API (v1)'});
-});
 
 app.use(express.json());
-app.use(router);
+app.use('/api/v1', MainRouter);
 
 app.listen(port, () => {
   return console.log(`⚡️ Express server is running at http://localhost:${port}`);
